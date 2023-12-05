ea1556e0c6f2d6a83c4ad1bd93fd6507cb6be873
menelli <menellialex@gmail.com>
2023-12-05 09:53:19 -0500

    Store page functional

--- storeprofile.php
+++ storeprofile.php
@@ -18,6 +18,90 @@
     <body>
         <?php require('Shared\header.php'); ?>
 
+        
+        <div class="heading">
+            <?php
+                echo("<h1>Welcome to the store profile page " . $_SESSION["Name"] .  "</h1>")
+            ?>
+        </div>
+
+        <div class="righttable">
+            <h2>Sold books</h2>
+
+            <table>
+                <tr>
+                    <th>Book</th>
+                    <th>Customer</th>
+                    <th>Price</th>
+                </tr>
+
+                <?php
+                    #return all wishlisted book titles
+                    if ($result = $conn->query("select b.name, s.emailaddr, s.price from sells s, books b where b.ISBN = s.ISBN and s.StoreID = b.StoreID and s.StoreID = '" . $_SESSION["StoreID"] . "';"))
+                    {
+                        #check if we returned any number of rows
+                        if ($result->num_rows > 0)
+                        {
+                            #if there are rows greater than 0, iterate through them.
+                            while($row = $result->fetch_row())
+                            {
+                                echo("<tr>");
+                                foreach ($row as $data)
+                                {
+                                    echo("<td>" . $data . "</td>");
+                                }
+                                echo("</tr>");
+                            }
+                        }
+                    }
+                    else
+                    {
+                        echo($conn->error);
+                    }
+                ?>
+            </table>
+
+        </div>
+
+        <div class="lefttable">
+            <h2>Current Rents</h2>
+
+            <table class="renttable">
+                <tr>
+                    <th>Book</th>
+                    <th>Customer</th>
+                    <th>Return Date</th>
+                </tr>
+                
+                
+                <?php
+                    #return all wishlisted book titles
+                    if ($result = $conn->query("SELECT b.name, c.emailaddr, r.renttill FROM rents r, books b, customer c WHERE b.ISBN = r.ISBN and r.EmailAddr = c.EmailAddr and r.StoreID = b.StoreID and r.StoreID = '" . $_SESSION["StoreID"] . "';"))
+                    {
+                        #check if we returned any number of rows
+                        if ($result->num_rows > 0)
+                        {
+                            #if there are rows greater than 0, iterate through them.
+                            while($row = $result->fetch_row())
+                            {
+                                echo("<tr>");
+                                foreach ($row as $data)
+                                {
+                                    echo("<td>" . $data . "</td>");
+                                }
+                                echo("</tr>");
+                            }
+                        }
+                    }
+                    else
+                    {
+                        echo($conn->error);
+                    }
+                ?>
+            </table>
+        </div>
+
+
         <?php require('Shared\footer.php'); ?>
     </body>
 </html>
